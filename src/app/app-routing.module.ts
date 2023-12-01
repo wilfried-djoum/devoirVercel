25702c31cfe35bd406614dc8fb609181f995dea2
WIlfried Djoum Nguemtchue <wilfried.djoum@institutsaintjean.org>
2023-12-01 20:23:44 +0100

    feat:ceci est mon nouveau projet github consistant en mettre en pratiques les techniques de CI-CD apprises en cours, cependant ce premier depot vise à me presenter et tester la pipeline d'integration continue en y ajoutant des tests de validation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfilComponent } from './profil/profil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "profil",
+    component:ProfilComponent
+  },
+  {
+    path: '',
+    component: ProfilComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/profil/profil.component.spec.ts
@@ -0,0 +1,41 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfilComponent } from './profil.component';
+
+describe('ProfilComponent', () => {
+  let component: ProfilComponent;
+  let fixture: ComponentFixture<ProfilComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProfilComponent]
+    });
+    fixture = TestBed.createComponent(ProfilComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("devrait restituer correctement le contenu du header", () => {
+    const headerContent = fixture.nativeElement.querySelector('.header-content');
+    expect(headerContent).toBeTruthy();
+  });
+
+  it("Ceci devrait contenir 5 icones", () => {
+    const icones = fixture.nativeElement.querySelectorAll('.controls .control i');
+    expect(icones.length).toBe(5)
+  })
+
+  it("devrait contenir mon nom", () => {
+    const nomuser = fixture.nativeElement as HTMLElement;
+    expect(nomuser.querySelector('.header .header-content .right-header .name .nom')?.textContent).toContain('Djoum Nguemtchue Wilfried.');
+  })
+
+  it("devrait contenir ma classe", () => {
+    const nomuser = fixture.nativeElement as HTMLElement;
+    expect(nomuser.querySelector('.header .header-content .right-header .name .classe')?.textContent).toContain('Ingenieur 4 ISI');
+  })
+});
